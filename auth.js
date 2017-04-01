6a2f936163d8939a22565d7aec531174f43deb4b
ShivamDh <shdh8888@yahoo.ca>
2017-04-01 06:01:08 -0400

    An authentication js file, didn't rlly use it

---
+++ auth.js
@@ -0,0 +1,5 @@
+var watson = require ('watson-developer-cloud');
+var visual_recognition = watson.visual_recognition({
+	api_key: '853a3ea7c77f8568884f31a0b3267f096e1c3275',
+	version_date: '2016-05-20'
+});
